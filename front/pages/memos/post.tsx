1848342d247c67df815dd43825e39469a5b17cdb
ryosuke_kamiya <Skyriver753@gmail.com>
2022-04-21 20:57:40 +0900

    [add]メモ機能追加

--- front/pages/memos/post.tsx
+++ front/pages/memos/post.tsx
@@ -1,35 +1,87 @@
-import type { NextPage } from 'next';
-import { RequiredMark } from '../../components/RequiredMark';
+import { AxiosError, AxiosResponse } from "axios";
+import type { NextPage } from "next";
+import { ChangeEvent, useState } from "react";
+import { RequiredMark } from "../../components/RequiredMark";
+import { axiosApi } from "../../lib/axios";
+
+// POSTデータの型
+type MemoForm = {
+  title: string;
+  body: string;
+};
 
 const Post: NextPage = () => {
+  // state定義
+  const [memoForm, setMemoForm] = useState<MemoForm>({
+    title: "",
+    body: "",
+  });
+  const [validation, setValidation] = useState<MemoForm>({
+    title: "",
+    body: "",
+  });
+
+  // POSTデータの更新
+  const updateMemoForm = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setMemoForm({ ...memoForm, [e.target.name]: e.target.value });
+  };
+
+  // メモの登録
+  const createMemo = () => {
+    axiosApi
+      // CSRF保護の初期化
+      .get("/sanctum/csrf-cookie")
+      .then((res) => {
+        // APIへのリクエスト
+        axiosApi
+          .post("/api/memos", memoForm)
+          .then((response: AxiosResponse) => {
+            console.log(response.data);
+          })
+          .catch((err: AxiosError) => {
+            console.log(err.response);
+          });
+      });
+  };
+
   return (
-    <div className='w-2/3 mx-auto'>
-      <div className='w-1/2 mx-auto mt-32 border-2 px-12 py-16 rounded-2xl'>
-        <h3 className='mb-10 text-2xl text-center'>メモの登録</h3>
-        <div className='mb-5'>
-          <div className='flex justify-start my-2'>
+    <div className="w-2/3 mx-auto">
+      <div className="w-1/2 mx-auto mt-32 border-2 px-12 py-16 rounded-2xl">
+        <h3 className="mb-10 text-2xl text-center">メモの登録</h3>
+        <div className="mb-5">
+          <div className="flex justify-start my-2">
             <p>タイトル</p>
             <RequiredMark />
           </div>
           <input
-            className='p-2 border rounded-md w-full outline-none'
-            name='title'
+            className="p-2 border rounded-md w-full outline-none"
+            name="title"
+            value={memoForm.title}
+            onChange={updateMemoForm}
           />
         </div>
-        <div className='mb-5'>
-          <div className='flex justify-start my-2'>
+        <div className="mb-5">
+          <div className="flex justify-start my-2">
             <p>メモの内容</p>
             <RequiredMark />
           </div>
+          　　　　　　{/* value属性とonChange属性を追加 */}
           <textarea
-            className='p-2 border rounded-md w-full outline-none'
-            name='body'
+            className="p-2 border rounded-md w-full outline-none"
+            name="body"
             cols={30}
             rows={4}
+            value={memoForm.body}
+            onChange={updateMemoForm}
           />
         </div>
-        <div className='text-center'>
-          <button className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 mt-8 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'>
+        <div className="text-center">
+          <button
+            onClick={createMemo}
+            className="bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 mt-8 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600"
+          >
             登録する
           </button>
         </div>
