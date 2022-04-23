1bb36554623ed76fbe4bf2e09e83d39f67a60f0a
ryosuke_kamiya <Skyriver753@gmail.com>
2022-04-23 23:04:24 +0900

    [add]recoil導入

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useState } from "react";
 import { RequiredMark } from "../components/RequiredMark";
 import { axiosApi } from "../lib/axios";
 import { useRouter } from "next/router";
+import { useUserState } from "../atoms/userAtom";
 
 // POSTデータの型
 type LoginForm = {
@@ -12,7 +13,11 @@ type LoginForm = {
 };
 
 // バリデーションメッセージの型
-type Validation = LoginForm & { loginFailed: string };
+type Validation = {
+  email?: string;
+  password?: string;
+  loginFailed?: string;
+};
 
 const Home: NextPage = () => {
   const router = useRouter();
@@ -32,8 +37,12 @@ const Home: NextPage = () => {
     setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
   };
 
+  const { setUser } = useUserState();
+
   // ログイン
   const login = () => {
+    // バリデーションメッセージの初期化
+    setValidation({});
     axiosApi
       // CSRF保護の初期化
       .get("/sanctum/csrf-cookie")
@@ -43,10 +52,26 @@ const Home: NextPage = () => {
           .post("/login", loginForm)
           .then((response: AxiosResponse) => {
             console.log(response.data);
+            setUser(response.data.data);
             router.push("/memos");
           })
           .catch((err: AxiosError) => {
-            console.log(err.response);
+            // バリデーションエラー
+            if (err.response?.status === 422) {
+              // ここから追加
+              const errors = err.response?.data.errors;
+              // state更新用のオブジェクトを別で定義
+              const validationMessages: { [index: string]: string } =
+                {} as Validation;
+              Object.keys(errors).map((key: string) => {
+                validationMessages[key] = errors[key][0];
+              });
+              // state更新用オブジェクトに更新
+              setValidation(validationMessages);
+            }
+            if (err.response?.status === 500) {
+              alert("システムエラーです！！");
+            }
           });
       });
   };
@@ -66,7 +91,9 @@ const Home: NextPage = () => {
             value={loginForm.email}
             onChange={updateLoginForm}
           />
-          {/* <p className='py-3 text-red-500'>必須入力です。</p> */}
+          {validation.email && (
+            <p className="py-3 text-red-500">{validation.email}</p>
+          )}
         </div>
         <div className="mb-5">
           <div className="flex justify-start my-2">
@@ -83,14 +110,14 @@ const Home: NextPage = () => {
             value={loginForm.password}
             onChange={updateLoginForm}
           />
-          {/* <p className='py-3 text-red-500'>
-            8文字以上の半角英数字で入力してください。
-          </p> */}
+          {validation.password && (
+            <p className="py-3 text-red-500">{validation.password}</p>
+          )}
         </div>
         <div className="text-center mt-12">
-          {/* <p className='py-3 text-red-500'>
-            IDまたはパスワードが間違っています。
-          </p> */}
+          {validation.loginFailed && (
+            <p className="py-3 text-red-500">{validation.loginFailed}</p>
+          )}
           <button
             className="bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600"
             onClick={login}
